5745b15bfa0ca1241d9acd2e99bdae19248cea4a
danzhang8962 <zhangdan8962@gmail.com>
2014-10-19 16:47:10 -0400

    stemming

--- hw2/instructor/src/edu/nyu/cs/cs2580/IndexerInvertedCompressed.java
+++ hw2/instructor/src/edu/nyu/cs/cs2580/IndexerInvertedCompressed.java
@@ -126,7 +126,8 @@ public class IndexerInvertedCompressed extends Indexer implements Serializable{
   private int readPostingList(String content,int docid,int offset, HashMap<String,Integer> counts) {
     Scanner s = new Scanner(content);  // Uses white space by default.
     while (s.hasNext()) {
-      String token = s.next();
+      String word = s.next();
+      String token = stemming(word);
       
       if (invertedIndex.containsKey(token)) {
         ArrayList<Integer> pointer_list = pointers.get(token);
@@ -366,6 +367,55 @@ public class IndexerInvertedCompressed extends Indexer implements Serializable{
          return res;
   }
 }
+
+
+
+private String stemming(String tokens){
+    return stemming3(stemming2(stemming1(tokens)));
+  }
+
+//step 1, get rid of plurals
+  private String stemming1(String tokens){
+    if(tokens.length() > 1){
+      if(tokens.endsWith("s")){
+        if(tokens.charAt(tokens.length()-2) == 's'){
+          return tokens;
+        }else if(tokens.charAt(tokens.length()-2) =='e'){
+          //need to determine if "es" are plural, check if -ch,-x, -s or -ss
+          if(tokens.length()>3){
+            if(tokens.charAt(tokens.length()-3) == 'x' || tokens.charAt(tokens.length()-3) == 's' || 
+                  tokens.substring(tokens.length()-4, tokens.length()-2).equals("ch")){
+              tokens = tokens.substring(0, tokens.length()-2); //remove "es"
+              return tokens; 
+            }
+          }
+        }
+      tokens = tokens.substring(0, tokens.length()-1);
+      }
+    } 
+    return tokens;
+  }
+//step 2 remove ed(ly) and ing(ly)
+  private String stemming2(String tokens){
+    if(tokens.endsWith("ed")){
+      tokens = tokens.substring(0, tokens.length()-2);
+    }else if(tokens.endsWith("edly")){
+      tokens = tokens.substring(0, tokens.length()-4);
+    }else if(tokens.endsWith("ing")){
+      tokens = tokens.substring(0, tokens.length()-3);    
+    }else if(tokens.endsWith("ingly")){
+      tokens = tokens.substring(0, tokens.length()-5);
+    }
+    return tokens;
+  }
+
+//step3 turns –y to –i
+private String stemming3(String tokens){
+  if(tokens.endsWith("y")){
+    return tokens.substring(0, tokens.length()-1) + "i";
+  }
+  return tokens;
+}  
 
   /**
    * @CS2580: Implement this for bonus points.
